34f9912053649df9d8f6925726fb9810a3ff6411
Crowdbotics <team@crowdbotics.com>
2023-01-30 20:43:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "magic-meal-38581",
+  url: "https://magic-meal-38581.botics.co",
+  api: "https://magic-meal-38581.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
